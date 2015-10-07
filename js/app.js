72fa36c71b44a6bc154d363988cab436187e7012
AG <ann_gibson@bellsouth.net>
2015-10-06 23:02:30 -0400

    add js scripts on abt and index

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+$("#phototheme").change(function(){
+  location.href= jQuery(this).val();  
+    
+})
+
+
+$(".contactBox").load("https://mervgibsonimages.com/contactus.sc");
+
+
+
+});
+
+
